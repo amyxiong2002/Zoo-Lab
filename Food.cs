f8e66442023ca773db4de6d691084fa04d487308
amyxiong2002 <81616698+amyxiong2002@users.noreply.github.com>
2021-03-30 03:48:36 -0500

    Create Food.cs

---
+++ Food.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooScenario
+{
+    /// <summary>
+    /// The food the animal is eating.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public class Food
+    {
+        /// <summary>
+        /// Weight of the food.
+        /// </summary>
+        public double Weight;
+    }
+}
